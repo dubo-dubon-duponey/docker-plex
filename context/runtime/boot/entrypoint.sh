c40060d0b9cc8d35a5800298ff7fade1fdf00766
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-09-20 15:37:07 -0700

    Bump + minor caddy refactor
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/runtime/boot/entrypoint.sh
+++ context/runtime/boot/entrypoint.sh
@@ -27,11 +27,11 @@ case "${1:-run}" in
   ;;
   # Helper to get the ca.crt out (once initialized)
   "cert")
-    if [ "${TLS:-}" == "" ]; then
+    if [ "${TLS_MODE:-}" == "" ]; then
       printf >&2 "Your container is not configured for TLS termination - there is no local CA in that case."
       exit 1
     fi
-    if [ "${TLS:-}" != "internal" ]; then
+    if [ "${TLS_MODE:-}" != "internal" ]; then
       printf >&2 "Your container uses letsencrypt - there is no local CA in that case."
       exit 1
     fi
@@ -49,7 +49,7 @@ case "${1:-run}" in
     fi
 
     # If we want TLS and authentication, start caddy in the background
-    if [ "${TLS:-}" ]; then
+    if [ "${TLS_MODE:-}" ]; then
       HOME=/tmp/caddy-home caddy run -config /config/caddy/main.conf --adapter caddyfile &
     fi
   ;;
